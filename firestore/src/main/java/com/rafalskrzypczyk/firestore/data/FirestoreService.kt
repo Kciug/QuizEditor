342b0cee2f6b0352bb929da48cbf7b89e2c75332
RafalS <rafalskrzypczyk96@gmail.com>
2025-03-11 11:32:57 +0100

    refactor FirestoreService

--- firestore/src/main/java/com/rafalskrzypczyk/firestore/data/FirestoreService.kt
+++ firestore/src/main/java/com/rafalskrzypczyk/firestore/data/FirestoreService.kt
@@ -2,7 +2,6 @@ package com.rafalskrzypczyk.firestore.data
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.snapshots
 import com.rafalskrzypczyk.core.R
 import com.rafalskrzypczyk.core.api_result.Response
 import com.rafalskrzypczyk.core.utils.ResourceProvider
@@ -10,11 +9,10 @@ import com.rafalskrzypczyk.firestore.data.models.CategoryDTO
 import com.rafalskrzypczyk.firestore.data.models.QuestionDTO
 import com.rafalskrzypczyk.firestore.data.models.UserDataDTO
 import com.rafalskrzypczyk.firestore.domain.FirestoreApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -25,112 +23,82 @@ class FirestoreService @Inject constructor(
 ) : FirestoreApi {
     override fun getUserData(userId: String): Flow<Response<UserDataDTO>> = flow {
         emit(Response.Loading)
-
         val result = firestore.collection(FirestoreCollections.USER_DATA_COLLECTION)
             .document(userId)
             .get()
             .await()
             .toObject(UserDataDTO::class.java)
 
-        if (result != null)
-            emit(Response.Success(result))
-    }.catch {
-        emit(
-            Response.Error(
-                it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown)
-            )
-        )
-    }
+        emit(result?.let { Response.Success(it) } ?: Response.Error(resourceProvider.getString(R.string.error_no_data)))
+    }.catch { emit(Response.Error(it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))) }
 
-    override fun getQuizCategories(): Flow<Response<List<CategoryDTO>>> = channelFlow {
-        trySend(Response.Loading)
-        val collection = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
-        collection.get().await().toObjects(CategoryDTO::class.java).let {
-                trySend(Response.Success(it))
-            }
-    }.catch {
-        emit(
-            Response.Error(
-                it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown)
-            )
-        )
-    }
+    override fun getQuizCategories(): Flow<Response<List<CategoryDTO>>> = flow {
+        emit(Response.Loading)
+        val categories = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
+            .get().await()
+            .toObjects(CategoryDTO::class.java)
+        emit(Response.Success(categories))
+    }.catch { emit(Response.Error(it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))) }
 
     override fun getUpdatedQuizCategories(): Flow<List<CategoryDTO>> = callbackFlow {
-        val collection = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
-        collection.snapshots().collectLatest {
-            if (it.isEmpty.not()) {
-                val categories = it.toObjects(CategoryDTO::class.java)
-                trySend(categories)
-            }
+        val listener = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
+            .addSnapshotListener { value, error ->
+                if(error != null) {
+                    close(error)
+                    return@addSnapshotListener
+                }
+                value?.let { trySend(it.toObjects(CategoryDTO::class.java)) }
         }
+        awaitClose { listener.remove() }
     }
 
-    override suspend fun addQuizCategory(category: CategoryDTO): Response<Unit> {
-        return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
-                .document(category.id.toString())
-                .set(category)
-                .await()
-            Response.Success(Unit)
-        } catch (e: Exception) {
-            Response.Error(e.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))
-        }
-    }
+    override suspend fun addQuizCategory(category: CategoryDTO): Response<Unit> =
+        addFirestoreDocument(category.id.toString(), category, FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
 
-    override suspend fun updateQuizCategory(category: CategoryDTO): Response<Unit> {
-        return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
-                .document(category.id.toString())
-                .set(category, SetOptions.merge())
-                .await()
-            Response.Success(Unit)
-        } catch (e: Exception) {
-            Response.Error(e.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))
-        }
-    }
+    override suspend fun updateQuizCategory(category: CategoryDTO): Response<Unit> =
+        modifyFirestoreDocument(category.id.toString(), category, FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
 
-    override suspend fun deleteQuizCategory(categoryId: Long): Response<Unit> {
-        return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
-                .document(categoryId.toString())
-                .delete()
-                .await()
-            Response.Success(Unit)
-        } catch (e: Exception) {
-            Response.Error(e.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))
-        }
-    }
+    override suspend fun deleteQuizCategory(categoryId: Long): Response<Unit> =
+        deleteFirestoreDocument(categoryId.toString(), FirestoreCollections.TEST_QUIZ_MODE_CATEGORIES)
 
-    override fun getQuizQuestions(): Flow<Response<List<QuestionDTO>>> = channelFlow {
-        trySend(Response.Loading)
-        val collection = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
-        collection.get().await().toObjects(QuestionDTO::class.java).let {
-            trySend(Response.Success(it))
-        }
-    }.catch {
-        emit(
-            Response.Error(
-                it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown)
-            )
-        )
-    }
+    override fun getQuizQuestions(): Flow<Response<List<QuestionDTO>>> = flow {
+        emit(Response.Loading)
+        val questions = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
+            .get().await()
+            .toObjects(QuestionDTO::class.java)
+        emit(Response.Success(questions))
+    }.catch { emit(Response.Error(it.localizedMessage ?: resourceProvider.getString(R.string.error_unknown))) }
 
     override fun getUpdatedQuizQuestions(): Flow<List<QuestionDTO>> = callbackFlow {
-        val collection = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
-        collection.snapshots().collectLatest {
-            if (it.isEmpty.not()) {
-                val questions = it.toObjects(QuestionDTO::class.java)
-                trySend(questions)
+        val listener = firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
+            .addSnapshotListener { value, error ->
+                if(error != null) {
+                    close(error)
+                    return@addSnapshotListener
+                }
+                value?.let { trySend(it.toObjects(QuestionDTO::class.java)) }
             }
-        }
+        awaitClose { listener.remove() }
     }
 
-    override suspend fun addQuizQuestion(question: QuestionDTO): Response<Unit> {
+    override suspend fun addQuizQuestion(question: QuestionDTO): Response<Unit> =
+        addFirestoreDocument(question.id.toString(), question, FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
+
+    override suspend fun updateQuizQuestion(question: QuestionDTO): Response<Unit> =
+        modifyFirestoreDocument(question.id.toString(), question, FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
+
+    override suspend fun deleteQuizQuestion(questionId: Long): Response<Unit> =
+        deleteFirestoreDocument(questionId.toString(), FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
+
+    private suspend fun <T : Any> addFirestoreDocument(
+        id: String,
+        data: T,
+        collection: String,
+    ): Response<Unit> {
         return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
-                .document(question.id.toString())
-                .set(question)
+            firestore.collection(collection)
+                .document(id)
+                .set(data)
                 .await()
             Response.Success(Unit)
         } catch (e: Exception) {
@@ -138,11 +106,15 @@ class FirestoreService @Inject constructor(
         }
     }
 
-    override suspend fun updateQuizQuestion(question: QuestionDTO): Response<Unit> {
+    private suspend fun <T : Any> modifyFirestoreDocument(
+        id: String,
+        data: T,
+        collection: String,
+    ): Response<Unit> {
         return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
-                .document(question.id.toString())
-                .set(question, SetOptions.merge())
+            firestore.collection(collection)
+                .document(id)
+                .set(data, SetOptions.merge())
                 .await()
             Response.Success(Unit)
         } catch (e: Exception) {
@@ -150,10 +122,13 @@ class FirestoreService @Inject constructor(
         }
     }
 
-    override suspend fun deleteQuizQuestion(questionId: Long): Response<Unit> {
+    private suspend fun deleteFirestoreDocument(
+        id: String,
+        collection: String
+    ): Response<Unit> {
         return try {
-            firestore.collection(FirestoreCollections.TEST_QUIZ_MODE_QUESTIONS)
-                .document(questionId.toString())
+            firestore.collection(collection)
+                .document(id)
                 .delete()
                 .await()
             Response.Success(Unit)
