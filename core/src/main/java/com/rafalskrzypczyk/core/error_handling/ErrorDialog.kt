3e2f7d2ce83b72be8faa00bcc58231eb7f4246a4
RafalS <rafalskrzypczyk96@gmail.com>
2025-03-13 21:11:05 +0100

    fix error dialogu

--- core/src/main/java/com/rafalskrzypczyk/core/error_handling/ErrorDialog.kt
+++ core/src/main/java/com/rafalskrzypczyk/core/error_handling/ErrorDialog.kt
@@ -2,6 +2,7 @@ package com.rafalskrzypczyk.core.error_handling
 
 import android.content.Context
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import com.rafalskrzypczyk.core.R
@@ -14,6 +15,11 @@ class ErrorDialog(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_error)
+
+        window?.setLayout(
+            (context.resources.displayMetrics.widthPixels * 0.9).toInt(),
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
 
         val messageTextView: TextView = findViewById(R.id.tv_error_message)
         val buttonClose: Button = findViewById(R.id.btn_dismiss)
