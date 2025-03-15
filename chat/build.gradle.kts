8db6b2ed852bd69cec77d91a2d407a88ebd97fb6
RafalS <rafalskrzypczyk96@gmail.com>
2025-03-15 20:16:46 +0100

    chat module setup

---
+++ chat/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    `android-library`
+    `kotlin-android`
+}
+
+apply<SharedGradleProjectConfig>()
+
+android {
+    namespace = "com.rafalskrzypczyk.auth"
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":auth"))
+    implementation(project(":firestore"))
+
+    coreKtx()
+    tests()
+    ui()
+    daggerHilt()
+}
