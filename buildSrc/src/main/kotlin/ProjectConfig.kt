7ede7f4246774f88b949820eadb68ec0025f9fac
RafalS <rafalskrzypczyk96@gmail.com>
2025-12-08 17:43:27 +0100

    - setup commit - version increment

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,9 +1,8 @@
-import org.gradle.api.JavaVersion
 
 object ProjectConfig {
     const val MIN_SDK = 24
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 10000
+    const val VERSION_NAME = "1.0.0"
 }
